56b503c10adbf7d0029841ae3c98daa1255614c6
carine ferreira da silva <carine.silvaf1@gmail.com>
2022-10-25 14:33:04 +1100

    commit tree b94b13702e5e7e8927c5ea261a5e4f68aa1fffb2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = "BOB"
+}
+
+
+
+function setBestCustomer(){
+   return 'not bob'
+}
